41d3725a08ed1b202afa29f9e7d8ca72e261c5ed
Ycros <michael@kedzierski.id.au>
2016-02-29 16:19:16 +1100

    Improved example code.

--- examples/internet_streams.py
+++ examples/internet_streams.py
@@ -8,10 +8,28 @@ path = f['path']
 start_with_http = path.left(7).stricmp('http://')
 strip_http = path.right(path.len() - 7)
 
+# get rid ef leading http://, then get rid of any port number
 domain = strip_http.left(strip_http.strchr('/') - 1)
 domain_no_port = domain.left(domain.strchr(':') - 1)
 
-script = f.if_(start_with_http, domain_no_port, path)
+# helpers
+domain_no_port_first_dot = domain_no_port.strchr('.')
+domain_no_port_last_dot = domain_no_port.strrchr('.')
+domain_right_part = domain_no_port.right(domain_no_port.len() - domain_no_port_last_dot)
+
+# TODO: the domain shortening logic is a huge hack, could be better
+# only shorten domain if the right most part isn't a TLD (at a guess)
+shortened_domain = f.ifequal(domain_right_part.len(), 2,
+                             domain_no_port,
+                             domain_no_port.right(domain_no_port.len() - domain_no_port_first_dot))
+
+# try to shorten the domain to get rid of subdomains
+shortened_if_domain = f.ifequal(domain_no_port_last_dot, domain_no_port_first_dot,
+                                domain_no_port,
+                                shortened_domain)
+
+# main body
+script = f.if_(start_with_http, shortened_if_domain, path)
 
 if __name__ == '__main__':
     # print(domain_no_port.output())
